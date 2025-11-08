5d76918a4b83a6eb0ed019d91603c6f3538275fa
ChecheRomo96 <josemanuel.romo.96@gmail.com>
2025-11-07 19:37:41 -0600

    .

---
+++ src/core/control/PID/PID.h
@@ -0,0 +1,12 @@
+#ifndef AVG_CORE_CONTROL_H
+#define AVG_CORE_CONTROL_H
+
+
+    namespace AGV_Core {
+        namespace Control {
+            // Aquí pueden ir utilidades generales de control
+        }
+    }
+
+
+#endif//AVG_CORE_CONTROL_H
---
+++ src/core/control/control.h
@@ -0,0 +1,6 @@
+#ifndef AVG_CORE_CONTROL_H
+#define AVG_CORE_CONTROL_H
+
+    #include <PID/PID.h>
+
+#endif//AVG_CORE_CONTROL_H
